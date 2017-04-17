85106c07f230e850eafe1e4e22a4146f2504d261
Mark Headd <mark.headd@gsa.gov>
2017-04-17 15:17:49 -0400

    Beautify

--- test/syracuse-airport-api.js
+++ test/syracuse-airport-api.js
@@ -6,74 +6,74 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('Syracuse Airport flight information API', () => {
-	before(() => {
-		server.start();
-	});
+    before(() => {
+        server.start();
+    });
 
-	describe('Tests', () => {
+    describe('Tests', () => {
 
-	  it('Should return valid JSON with GET (all flights)', (done) => {
-	  	chai.request(server.app)
-	  	.get('/')
-	  	    .end((err, res) => {
-		      res.should.have.status(200);
-		      res.should.be.json;
-		      res.body.should.be.a('array');
-		      res.body.should.have.lengthOf(1);
-		      done();
-		    });
-	  });
+        it('Should return valid JSON with GET (all flights)', (done) => {
+            chai.request(server.app)
+                .get('/')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.should.be.a('array');
+                    res.body.should.have.lengthOf(1);
+                    done();
+                });
+        });
 
-	  it('Should return valid JSON with GET (city)', (done) => {
-	  	chai.request(server.app)
-	  	.get('/city/toronto')
-	  	    .end((err, res) => {
-		      res.should.have.status(200);
-		      res.should.be.json;
-		      res.body.should.be.a('array');
-		      res.body.should.have.length.above(1);
-		      done();
-		    });
-	  });
+        it('Should return valid JSON with GET (city)', (done) => {
+            chai.request(server.app)
+                .get('/city/toronto')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.should.be.a('array');
+                    res.body.should.have.length.above(1);
+                    done();
+                });
+        });
 
-	  it('Should return valid JSON with GET (direction)', (done) => {
-	  	chai.request(server.app)
-	  	.get('/direction/arrival')
-	  	    .end((err, res) => {
-		      res.should.have.status(200);
-		      res.should.be.json;
-		      res.body.should.be.a('array');
-		      res.body.should.have.length.above(1);
-		      done();
-		    });
-	  });
+        it('Should return valid JSON with GET (direction)', (done) => {
+            chai.request(server.app)
+                .get('/direction/arrival')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.should.be.a('array');
+                    res.body.should.have.length.above(1);
+                    done();
+                });
+        });
 
-	  it('Should return valid JSON with GET (gate)', (done) => {
-	  	chai.request(server.app)
-	  	.get('/gate/5')
-	  	    .end((err, res) => {
-		      res.should.have.status(200);
-		      res.should.be.json;
-		      res.body.should.be.a('array');
-		      res.body.should.have.length.above(1);
-		      done();
-		    });
-	  });
+        it('Should return valid JSON with GET (gate)', (done) => {
+            chai.request(server.app)
+                .get('/gate/5')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.should.be.a('array');
+                    res.body.should.have.length.above(1);
+                    done();
+                });
+        });
 
-	  it('Should return valid JSON with GET (flight number)', (done) => {
-	  	chai.request(server.app)
-	  	.get('/number/4876')
-	  	    .end((err, res) => {
-		      res.should.have.status(200);
-		      res.should.be.json;
-		      res.body.should.be.a('array');
-		      done();
-		    });
-	  });
+        it('Should return valid JSON with GET (flight number)', (done) => {
+            chai.request(server.app)
+                .get('/number/4876')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.should.be.a('array');
+                    done();
+                });
+        });
 
-	});
+    });
 
-	after(() => {
-		server.stop();
-	});
+    after(() => {
+        server.stop();
+    });
 });
