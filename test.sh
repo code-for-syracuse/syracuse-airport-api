cf4b34b4e8ecd72a1402c8fdb828505ceaea9481
Mark Headd <mheadd@gmail.com>
2020-04-06 10:08:53 -0400

    remove dependency

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Start local wb server to serve test xml.
-./node_modules/http-server/bin/http-server ./data > /dev/null 2>&1 &
+# Start local web server to serve test xml.
+php -S 127.0.0.1:8080 -t ./data &
 
 # Set env variable and run tests.
 TESTING=1 ./node_modules/mocha/bin/mocha
 
 # Shut down test server
-pid=$(ps | grep http-server | head -n 1 | awk '{print $1}')
+pid=$(ps | grep php | head -n 1 | awk '{print $1}')
 kill -9 $pid
